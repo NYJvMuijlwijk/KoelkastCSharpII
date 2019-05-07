1eb1c6bac6ae5cbcedd0598069c652b179d99d29
Niek v M <microniek@live.nl>
2019-05-07 15:20:34 +0200

    migrations enabled

---
+++ KoelkastProject/Koelkast/Migrations/Configuration.cs
@@ -0,0 +1,26 @@
+namespace Koelkast.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using MySql.Data.MySqlClient;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Koelkast.Models.MySQL>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            SetSqlGenerator("MySql.Data.MySqlClient", new MySql.Data.Entity.MySqlMigrationSqlGenerator());
+
+        }
+
+        protected override void Seed(Koelkast.Models.MySQL context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
