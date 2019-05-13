516b6f02e9fa7a3c7ad5f78e831fd61bc89ef815
tiddo vlek <tvlek@roc-dev.nl>
2019-05-13 12:05:42 +0200

    User/Fride model

---
+++ KoelkastProject/Koelkast/Models/Fridge.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Koelkast.Models
+{
+    // Models returned by MeController actions.
+    public class User
+    {
+        public string Hometown { get; set; }
+    }
+}
