fb3e9f22fedb964d5fb180c3ebc79910676f21e4
Niek <niekvanmuylwijck@gmail.com>
2019-05-16 18:15:44 +0200

    Fridge_Ingredient renamed, added Fridge and Ingredient prop for EF db relation

--- KoelkastProject/Koelkast/Models/Fridge_Ingredient_Amount.cs
+++ KoelkastProject/Koelkast/Models/Fridge_Ingredient_Amount.cs
@@ -5,12 +5,14 @@ using System.ComponentModel.DataAnnotations;
 namespace Koelkast.Models
 {
     // Models returned by MeController actions.
-    public class Fridge_Ingredient
+    public class Fridge_Ingredient_Amount
     {
-        [Key]
-        public int Id { get; set; }
-
         [Required]
         public int Amount { get; set; }
+
+        [Required]
+        public Fridge Fridge { get; set; }
+        [Required]
+        public Ingredient Ingredient { get; set; }
     }
 }
